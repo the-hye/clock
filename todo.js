56706b32c336214ac12983291298a6b4b0154760
the-hye <ganzi410@gmail.com>
2021-02-16 20:58:34 +0900

    for Each()/filter

--- todo.js
+++ todo.js
@@ -4,7 +4,23 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+function filterFn(toDos) {
+    return toDo.id === 1;
+}
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -17,6 +33,8 @@ function paintTodo(text) {
     const newId = toDos.length + 1
 
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
+
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
